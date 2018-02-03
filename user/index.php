6ae38561b3aca4e2352da46ca23dcc229bbb538a
9prashant-1223 <prashantverma1223@gmail.com>
2018-02-03 18:10:08 +0530

    admin api

--- user/index.php
+++ user/index.php
@@ -1,9 +1,20 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title></title>
-</head>
-<body>
-<h1 style="text-align: center;">User</h1>
+    <?php 
+        require_once 'header.php';
+     ?>
+    
+    <div class="container-fluid">
+        <div class="row" >
+            <div class="col-sm-6 col-sm-offset-5" style="text-align: center;margin-bottom:7vh;">
+
+                <a href="#login" data-toggle="modal" data-target="#login" class="rainbow-button" alt="Apply Now">apply</a>
+
+            </div>
+        </div>
+    </div>
+
+
+    <?php //require_once('footer.php'); ?>
+
+    <?php require_once('../components/login_modal.php'); ?>
+   
 </body>
-</html>
