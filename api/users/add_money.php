7da07043adca7aa43589289890204eec2d6a6bcf
9prashant-1223 <prashantverma1223@gmail.com>
2018-02-04 06:56:07 +0530

    api for adding money by user

--- api/users/add_money.php
+++ api/users/add_money.php
@@ -1,25 +1,44 @@
-<?php
-    require_once './../config/config.php';
+<?php 
+include '../../config/config.php';
 
-    $_SESSION['user_id'] = 1; 
-    $_SESSION['role'] = 'user';
 
-    $user_id = $_SESSION['user_id'];
-    
-    if(isset($_POST['payment'])){
-        $payment = $_POST['payment'];
-    }
+    $userId = $_POST['userId'];
+    $moneyAdd  = $_POST['moneyAdd'];
 
-    if($_SESSION['role'] == 'user' && isset($_POST['submit'])) {
-        $make_payment = "UPDATE users SET balance=balance+$payment WHERE id=$user_id;";
-        if($conn->query($make_payment)) {
-            print "Successfully Added";
-        } else {
-            print "money not added";
-        };
-    }
+    $sql = "SELECT balance from users where user_id=?";
+                if($stmt = mysqli_prepare($conn, $sql)){
+                    mysqli_stmt_bind_param($stmt, "ii", $param_user_id);
+                    
+                    $param_user_id = $userId;
+                    if(mysqli_stmt_execute($stmt)){
+                        mysqli_stmt_store_result($stmt);
+
+                        if(!mysqli_stmt_num_rows($stmt) == 0){                    
+                            mysqli_stmt_bind_result($stmt, $balance);
+                            if(mysqli_stmt_fetch($stmt)){
+                                $balance = $balance + $moneyAdd;
+                                $sql = "UPDATE users set balance=? where user_id=?";
+         
+                                    if($stmt = mysqli_prepare($conn, $sql)){
+                                        mysqli_stmt_bind_param($stmt, "ii",$param_balance, $param_user_id);
+                                        $param_balance = $balance;
+                                        if(mysqli_stmt_execute($stmt)){
+                                            echo '<script>alert("Money Added .")</script>';
+                                        } else{
+                                            echo '<script>alert("Something Went Wrong.")</script>';
+                                        }
+                                    }else {echo '<script>alert("Something Went Wrong.")</script>';}
+                                     
+                                    mysqli_stmt_close($stmt);
+                                    
+                                }
+                                else{
+                                echo '<script>alert("Something Went Wrong.")</script>';
+                                }
+                            }
+                        }
+                    mysqli_stmt_close($stmt);
+                
+            mysqli_close($conn);
+        }
 ?>
-<form action="" method="POST">
-    <input type="number" name="payment" />
-    <input type="submit" name="submit" />
-</form> 
