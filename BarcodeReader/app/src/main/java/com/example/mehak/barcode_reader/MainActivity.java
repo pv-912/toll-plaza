6aab46c6fe79ae27f156069b93ec3c36075d1cac
mehak0108 <mehakmittal63@yahoo.in>
2018-02-06 02:39:02 +0530

    App added

---
+++ BarcodeReader/app/src/main/java/com/example/mehak/barcode_reader/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.mehak.barcode_reader;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.util.SparseArray;
+import android.widget.Toast;
+
+import com.google.android.gms.vision.barcode.Barcode;
+
+import java.util.List;
+
+import info.androidhive.barcode.BarcodeReader;
+
+public class MainActivity extends AppCompatActivity implements BarcodeReader.BarcodeReaderListener {
+
+    BarcodeReader barcodeReader;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // get the barcode reader instance
+        barcodeReader = (BarcodeReader) getSupportFragmentManager().findFragmentById(R.id.barcode_scanner);
+    }
+
+    @Override
+    public void onScanned(Barcode barcode) {
+
+        barcodeReader.playBeep();
+        Log.v("Value",barcode.displayValue);
+
+    }
+
+    @Override
+    public void onScannedMultiple(List<Barcode> barcodes) {
+
+    }
+
+    @Override
+    public void onBitmapScanned(SparseArray<Barcode> sparseArray) {
+
+    }
+
+    @Override
+    public void onScanError(String errorMessage) {
+
+        Toast.makeText(getApplicationContext(), "Error occurred while scanning " + errorMessage, Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void onCameraPermissionDenied() {
+
+        finish();
+    }
+}
