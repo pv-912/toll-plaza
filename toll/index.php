81b86c3dea8d79f69ee38ec312de76fc76a2f56d
9prashant-1223 <prashantverma1223@gmail.com>
2018-02-07 02:01:00 +0530

    header

--- toll/index.php
+++ toll/index.php
@@ -3,27 +3,15 @@ include 'header.php';
 ?>
       <div class="container-fluid">
         <div class="row">
-          <div class="col-sm-12">
-            <table class="table table-hover">
-              <tr>
-                <th>Vehicle Type</th>
-                <th>Vehicle No.</th>
-                <th>Vehicle Color</th>
-                <th>Vehicle Load</th>
-              </tr>
-              
-
-              
-            </table>
-          </div>
+          
         </div>
     </div>
 
 
-    <?php //require_once('footer.php'); ?>
+
+
 
     <?php require_once('../components/login_modal_tolls.php'); ?>
-   
 </body>
 
 
