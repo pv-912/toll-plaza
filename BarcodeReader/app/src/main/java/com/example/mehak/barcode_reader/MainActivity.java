72e4925df5f25cf5c0c9a9cae80573dd14304e42
mehak0108 <mehakmittal63@yahoo.in>
2018-02-07 16:39:48 +0530

    Post request added

--- BarcodeReader/app/src/main/java/com/example/mehak/barcode_reader/MainActivity.java
+++ BarcodeReader/app/src/main/java/com/example/mehak/barcode_reader/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.mehak.barcode_reader;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,6 +9,12 @@ import android.widget.Toast;
 
 import com.google.android.gms.vision.barcode.Barcode;
 
+import java.io.BufferedOutputStream;
+import java.io.BufferedWriter;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.List;
 
 import info.androidhive.barcode.BarcodeReader;
@@ -29,6 +36,8 @@ public class MainActivity extends AppCompatActivity implements BarcodeReader.Bar
 
         barcodeReader.playBeep();
         Log.v("Value",barcode.displayValue);
+        CallAPI callTask = new CallAPI();
+        callTask.execute(barcode.displayValue);
 
     }
 
@@ -53,5 +62,58 @@ public class MainActivity extends AppCompatActivity implements BarcodeReader.Bar
     public void onCameraPermissionDenied() {
 
         finish();
+    }
+
+    public class CallAPI extends AsyncTask<String, String, String> {
+
+        public CallAPI(){
+            //set context variables if required
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+
+        @Override
+        protected String doInBackground(String... param) {
+
+            String urlString = "http://spark.iitr.ac.in/toll-plaza/api/toll/comingVehicle.php"; // URL to call
+
+            String data = param[0]; //data to post
+
+            OutputStream out = null;
+            try {
+
+                URL url = new URL(urlString);
+
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+
+                out = new BufferedOutputStream(urlConnection.getOutputStream());
+
+                BufferedWriter writer = new BufferedWriter (new OutputStreamWriter(out, "UTF-8"));
+
+                writer.write(data);
+
+                writer.flush();
+
+                writer.close();
+
+                out.close();
+
+                urlConnection.connect();
+
+
+            } catch (Exception e) {
+
+                System.out.println(e.getMessage());
+
+
+
+            }
+            return null;
+
+        }
     }
 }
