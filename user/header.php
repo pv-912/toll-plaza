f1eb4d7a351967d66bbf7311cb899746aaa2c5d5
Varun Bhandia <varunbhandia@gmail.com>
2018-03-20 13:09:28 +0530

    uiux

--- user/header.php
+++ user/header.php
@@ -88,6 +88,7 @@
     <link href="<?php echo base_url; ?>src/css/bootstrap-theme.min.css" rel="stylesheet">
     <link href="<?php echo base_url; ?>src/css/style.css" rel="stylesheet">
     <script src="<?php echo base_url; ?>src/js/bootstrap.min.js"></script>
+      <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,600,900" rel="stylesheet">
   </head>
   <body>
    <nav class="navbar">
