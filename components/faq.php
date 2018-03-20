6eef411a9a633fe34021875fd025acf581e9a0a1
Varun Bhandia <varunbhandia@gmail.com>
2018-03-20 11:55:23 +0530

    faq page done

--- components/faq.php
+++ components/faq.php
@@ -13,9 +13,9 @@
             <div class="row" id="objectives">
                 <div class="container ">
                     <ol>
-                        <li><p class="faqQuestions text-justify">What are the benefits of using Pay-toll?</p></li>
+                        <li><p class="faqQuestions">What are the benefits of using Pay-toll?</p></li>
                         
-                        <ul>
+                        <ul class="faqAnswer">
                             <li>Cashless payment – Pay-toll users don’t need to carry cash for toll transactions, anddon’t need to worry about collecting the exact change.</li>
                             <li>Promotional cashback – All Pay-toll users get a monthly cashback of 10% of toll transactions till 31 March, 2017</li>
                             <li>Pay-toll transit – Auto-debit of exact amount through the FASPay-toll enables faster transit through the toll plazas and saves time</li>
@@ -24,7 +24,7 @@
                         </ul>
                     
                         <li><p class="faqQuestions">I have two vehicles, can I use one Pay-toll for two vehicles?</p></li>
-                        <ul>
+                        <ul class="faqAnswer">
                             <li>No, you will need to take two separate Pay-Toll for the two vehicles.</li>
                         </ul>
                     </ol>
@@ -32,3 +32,36 @@
             </div>
         </div>
         </div>
+<div id="contact" class="footer">
+    <div class="row">
+        <div class="col-md-6">
+            <div class="footer_header">
+                CONTACT US
+            </div>
+            <div class="half_underline_footer"></div>
+            <div class="email">
+                <p>Email Id : varunbhandia@tollplaza.com</p>
+                <p>Mobile No. : +91 9001126303</p>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="row">
+                    <div class="col-xs-4">
+                        <center>
+                        <a href="#"><img  src="../src/img/fb.png" alt="Sangram facebook" class="socialicons1"></a>
+                            </center>
+                    </div>
+                    <div class="col-xs-4">  
+                        <center>
+                        <a href="#"> <img src="../src/img/youtube.png" alt="Sangram youtube" class="socialicons2"></a>
+                        </center>
+                    </div>
+                    <div class="col-xs-4">   
+                        <center>
+                        <a href="#"> <img src="../src/img/insta.png" alt="Sangram instagram" class="socialicons3"></a>
+                        </center>
+                    </div>
+                </div>
+        </div>
+    </div>
+</div>
